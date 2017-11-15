9622777c49d782e30d58b078f893d75e70daf00c
Argir Argirov <dj.giro@gmail.com>
2017-11-15 15:48:22 -0500

    ajax call to receive list of candidates, display all of them with loop

--- election.js
+++ election.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET'
+  }).done(function (response) {
+    var list = document.querySelector('#list')
+    var candidates = response.candidates
+
+    for (var i = 0; i < candidates.length; i++) {
+
+      var item = document.createElement('li')
+      item.innerHTML = `${candidates[i].name}: ${candidates[i].votes} votes`
+      list.append(item)
+
+    }
+  })
 
 });
